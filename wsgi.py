17b26e5b1a79989e12ca77630877b580ef538a8d
Basivireddy <d.basivireddy@gmail.com>
2020-12-17 19:00:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import time
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    time.sleep(1)
     return "Hello World!"
 
 if __name__ == "__main__":
